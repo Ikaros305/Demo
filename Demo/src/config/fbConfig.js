84594e837f003e407da0de105b4454d740cfcf7d
Shubham Shirke <48343022+Ikaros305@users.noreply.github.com>
2021-10-21 15:46:25 +0530

    add

--- Demo/src/config/fbConfig.js
+++ Demo/src/config/fbConfig.js
@@ -4,13 +4,13 @@ import "firebase/auth";
 
 // Replace this with your own config details
 var config = {
-  apiKey: "AIzaSyA0CW8SXVIjnbBa2q0oi2X_YvqbxFIT8o4",
-  authDomain: "react-redux-firebase-c16dc.firebaseapp.com",
-  databaseURL: "https://react-redux-firebase-c16dc.firebaseio.com",
-  projectId: "react-redux-firebase-c16dc",
+  apiKey: "xxxxxxxxxxxx",
+  authDomain: "xxxxxxxxxxxxxxxxxxxxx",
+  databaseURL: "xxxxxxxxxxxxxxxxxxxxx",
+  projectId: "xxxxxxxxxxxxxxxxxx",
   storageBucket: "",
-  messagingSenderId: "640768633523",
-  appId: "1:640768633523:web:1c0950f9dde8657e"
+  messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 };
 firebase.initializeApp(config);
 firebase.firestore().settings({ timestampsInSnapshots: true });
